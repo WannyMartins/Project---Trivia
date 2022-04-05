80d9acf1b6dfeaa8c83a4984185acb8ed8c660cb
Wanny Martins <wcatery@gmail.com>
2022-04-05 18:58:52 -0300

    requisto 4 header iniciando

---
+++ src/components/Heder.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Heder extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1 data-testid="settings-title">Configurações</h1>
+      </header>
+    );
+  }
+}
+
+export default Heder;
