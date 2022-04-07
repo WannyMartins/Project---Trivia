c2e2c3a1728defe9b04987d0fc1f7d524c167450
Arthur Barros <artmorraes1@gmail.com>
2022-04-07 17:18:52 -0300

    REQUISITO 5 CONCLUIDO COM MUITO ÓDIO E VIGOR! AS VEZES LÁGRIMAS

--- src/components/Jogo.jsx
+++ src/components/Jogo.jsx
@@ -4,20 +4,26 @@ import { connect } from 'react-redux';
 import { tokenRequestAPI } from '../actions';
 import Header from './Header';
 
+const magic = 0.5;
+
 class Jogo extends Component {
   constructor() {
     super();
 
     this.state = {
       index: 0,
+      disableButton: false,
     };
   }
 
   renderQuestion = () => {
     const { results } = this.props;
-    const { index } = this.state;
-    const testando = document.querySelectorAll('.buttons');
-    console.log(testando);
+    const { index, disableButton } = this.state;
+    const correctAnswer = results[index].correct_answer;
+    const incorrectAnswers = (results[index].incorrect_answers);
+    const allAnswers = incorrectAnswers.concat(results[index].correct_answer);
+    allAnswers.sort(() => Math.random() - magic);
+    console.log(allAnswers);
     return (
       <div>
         <h1 data-testid="question-category">{results[index].category}</h1>
@@ -27,46 +33,67 @@ class Jogo extends Component {
             .replace(/&lt;/g, '<')
             .replace(/&gt;/g, '>')
             .replace(/&quot;/g, '"')
-            .replace(/&#039;/g, '\'') }
+            .replace(/&#039;/g, '\'')}
         </h3>
-        {
-          results[index].incorrect_answers.map((incorrect, position) => (
-            <button
-              type="button"
-              key={ position }
-              data-testid={ `wrong-answer-${position}` }
-              onClick={ console.log('ERRADA') }
-              className="buttons"
-            >
-              {incorrect}
-            </button>
-          ))
-        }
-        <button
-          type="button"
-          data-testid="correct-answer"
-          className="buttons"
-        >
-          {results[index].correct_answer}
-        </button>
+        <div data-testid="answer-options">
+          {
+            allAnswers.map((answer, position) => (
+              correctAnswer.includes(answer)
+                ? (
+                  <button
+                    type="button"
+                    key={ position }
+                    data-testid="correct-answer"
+                    className="buttons"
+                  >
+                    {
+                      answer
+                        .replace(/&amp;/g, '&')
+                        .replace(/&lt;/g, '<')
+                        .replace(/&gt;/g, '>')
+                        .replace(/&quot;/g, '"')
+                        .replace(/&#039;/g, '\'')
+                    }
+                  </button>)
+                : (
+                  <button
+                    type="button"
+                    key={ position }
+                    data-testid={ `wrong-answer-${position}` }
+                    className="buttons"
+                  >
+                    {answer
+                      .replace(/&amp;/g, '&')
+                      .replace(/&lt;/g, '<')
+                      .replace(/&gt;/g, '>')
+                      .replace(/&quot;/g, '"')
+                      .replace(/&#039;/g, '\'')}
+                  </button>)
+            ))
+          }
+        </div>
         <button
           type="button"
           data-testid="next-button"
-          onClick={ this.test }
+          disabled={ disableButton }
+          onClick={ this.nextQuestion }
         >
           NEXT
         </button>
       </div>
+
     );
   }
 
-  test = () => {
+  nextQuestion = () => {
     const { results } = this.props;
     const { index } = this.state;
+    const numberQuestion = 3;
     if (results.length - 1 !== index
-    && this.setState((previousValue) => ({ index: previousValue.index + 1 }))); // Função conjunta para aumentar o valor do ID // e não passa da ultima posição
+      && this.setState((previousValue) => ({ index: previousValue.index + 1 }))); // Função conjunta para aumentar o valor do ID // e não passa da ultima posição
+    if (index === numberQuestion) { this.setState({ disableButton: true }); }
     console.log(index);
-  }
+  };
 
   render() {
     const { results } = this.props;
@@ -74,7 +101,7 @@ class Jogo extends Component {
     return (
       <>
         <Header />
-        { results !== undefined && this.renderQuestion() }
+        {results !== undefined && this.renderQuestion()}
       </>
     );
   }
