da7e2f27a2a300d5ea76c72930a150fc6b548e24
Arthur Barros <artmorraes1@gmail.com>
2022-04-08 15:40:53 -0300

    Iniciando requisito e adicionando botão de inicio a aba de configurações para facilitar o redirecionamento para tela inicial

--- src/components/Configuracoes.jsx
+++ src/components/Configuracoes.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 class Configuracoes extends React.Component {
   render() {
     return (
       <header>
         <h1 data-testid="settings-title">Configurações</h1>
+        <Link to="/">
+          <button
+            type="button"
+          >
+            Inicio
+          </button>
+        </Link>
+
       </header>
     );
   }
